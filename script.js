45e52295ff289a1c9c99c95d3dfe86ed44f9d1a5
syedhasnain01 <syedhasnainraza2ai@gmail.com>
2024-11-30 19:52:14 +0500

    js

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(){
+    let num1 = prompt("enter your first number")
+    let num2 = prompt("enter your second number")
+
+    num1 = Number(num1)
+    num2 = Number(num2)
+
+    alert(num1+num2)
+}
+function subtract(){
+    let num1 = prompt("enter your first number")
+    let num2 = prompt("enter your second number")
+
+    num1 = Number(num1)
+    num2 = Number(num2)
+
+    alert(num1-num2)
+}
+function multiply(){
+    let num1 = prompt("enter your first number")
+    let num2 = prompt("enter your second number")
+
+    num1 = Number(num1)
+    num2 = Number(num2)
+
+    alert(num1*num2)
+}
+function divide(){
+    let num1 = prompt("enter your first number")
+    let num2 = prompt("enter your second number")
+
+    num1 = Number(num1)
+    num2 = Number(num2)
+
+    alert(num1/num2)
+}
